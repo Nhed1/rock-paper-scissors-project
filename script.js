b252200b1b2f8a72328d0787155a1cde6efd8eaa
Leonardo Garreto <leonardogarreto@gmail.com>
2021-10-28 10:34:30 -0300

    Just need to return the option from the addEventListener

--- script.js
+++ script.js
@@ -9,20 +9,26 @@ const whoWon = function(playerWon){
     }
     return result
 }
+
+const pcRandom = function(){
+    return Math.floor(Math.random() * 3) + 1;
+};
     
 let choices = ["Rock", "Paper", "Scissor"],
     pcChoice = choices[pcRandom -1],
     userChoices = document.querySelectorAll('.buttons')
 
+
 userChoices.forEach(choice => {
     choice.addEventListener('click', function(e){
-        console.log(e)
+        let choiceClass = e.path[0].className.split(""),
+        // take the last value of class name, it's the number of the option
+        option = parseInt(choiceClass[choiceClass.length-1])
+        console.log(option)
+
+        return option
     })
 });
-
-const pcRandom = function(){
-    return Math.floor(Math.random() * 3) + 1;
-};
 
 
 function checkResult() {
