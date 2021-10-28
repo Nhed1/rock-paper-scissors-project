56416bad65e51d16ab46a7cc7b97fba53b6fcc08
Leonardo Garreto <leonardogarreto@gmail.com>
2021-10-28 11:15:03 -0300

    still not get the user option of the function

--- script.js
+++ script.js
@@ -16,19 +16,39 @@ const pcRandom = function(){
     
 let choices = ["Rock", "Paper", "Scissor"],
     pcChoice = choices[pcRandom -1],
-    userChoices = document.querySelectorAll('.buttons')
+    userChoices = document.querySelectorAll('.option')
 
+let option = 0;
+
+const getOption = function(e) {
+    let choiceClass = e.path[0].className.split(""),
+    // take the last value of class name, it's the number of the option
+    option = parseInt(choiceClass[choiceClass.length-1])
+    // console.log(option)
+
+    return option
+
+}
 
 userChoices.forEach(choice => {
-    choice.addEventListener('click', function(e){
-        let choiceClass = e.path[0].className.split(""),
-        // take the last value of class name, it's the number of the option
-        option = parseInt(choiceClass[choiceClass.length-1])
-        console.log(option)
+    choice.addEventListener('click', getOption)
+})
 
-        return option
-    })
-});
+
+
+
+
+// userChoices.forEach(choice => {
+//     choice.addEventListener('click', function(e){
+//         let choiceClass = e.path[0].className.split(""),
+//         // take the last value of class name, it's the number of the option
+//         option = parseInt(choiceClass[choiceClass.length-1])
+
+//         return option
+//     })
+    
+// });
+
 
 
 function checkResult() {
