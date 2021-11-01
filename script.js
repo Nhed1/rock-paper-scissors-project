5d9b0bb19c0be760129b0d353f3f9c8bcf9afe6f
Leonardo Garreto <leonardogarreto@gmail.com>
2021-11-01 09:52:37 -0300

    problem when I had to remove the last item after the other is added

--- script.js
+++ script.js
@@ -1,81 +1,83 @@
-
-let choices = ['rock', 'scissors', 'paper']
-    Useroption = 0,
-    userChoices = document.querySelectorAll('button'),
-    displayWin = document.querySelector('.whoWon'),
-    displayPlayer = document.querySelector('.playerDiv'),
-    displayPC = document.querySelector('.pcDiv')
-
-function addChoices(choice,valuePc){
-    let element = document.createElement('i')
-    element.classList.add('fas')
-    element.classList.add('fa-hand-'+choices[choice-1])
-    displayPlayer.appendChild(element)
-
-    let elementPc = document.createElement('i')
-    elementPc.classList.add('fas')
-    elementPc.classList.add('fa-hand-'+choices[valuePc-1])
-    console.log(valuePc,choice)
-    displayPC.appendChild(elementPc)
-}
-
-// displayPC.append(rock)
-
-const whoWon = function(playerWon){
-    let result;
-    if (playerWon == "tie"){
-        result = "TIE!"
-    } else if (playerWon) {
-        result = "Player Won!"
-    } else {
-        result = "PC Won!"
-    }
-    return result
-}
+let pcDiv = document.querySelector('.pcDiv')
+let playerDiv = document.querySelector('.playerDiv')
+let buttons = document.querySelectorAll('button')
 
 const pcRandom = function(){
     let num = Math.floor(Math.random() * 3) + 1
         // pcChoice = choices[num -1]
-    return num
+    buttons.forEach(button => {
+        if (button.className == num) {
+            // take the class of the icon to the random num
+            pcChoice = button.firstChild.className
+        }
+    })
+    return pcChoice
 };
-    
-function checkResult(userChoice, pcChoice) {
-    if (userChoice === pcChoice) {
-        return whoWon("tie");
-    
-    } else if (userChoice === 3 && pcChoice === 1) {
-        return whoWon(false)
-    
-    } else if (userChoice === 1 && pcChoice === 3) {
-        return whoWon(true)
-    
-    } else if (userChoice > pcChoice) {
-        return whoWon(true)
-    
-    } else {
-        return whoWon(false)
-    }
-    
-}
 
-const getOption = function(e) {
-    e.stopPropagation(); 
-    let choiceClass = e.target.className
-    // take the last value of class name, it's the number of the option
-    Useroption = parseInt(choiceClass[choiceClass.length-1])
-    let valuePc = pcRandom()
-    addChoices(Useroption,valuePc)
-    let result = checkResult(Useroption, valuePc)
-    
-    console.log(result)
-}
+buttons.forEach(button => {
+    button.addEventListener('click', function(){
+        let playerChoice = button.firstChild.className
+        // take the class of the icon when click a button
+        let pcChoice = pcRandom()
+        pcChoiceSplit = pcChoice.split(' ')
+        playerChoiceSplit = playerChoice.split(' ')
+        appendElement(pcChoiceSplit, playerChoiceSplit)
 
-userChoices.forEach(choice => {
-    choice.addEventListener('click', getOption)
+    })
 })
 
+function appendElement(pc, player ){
+    let iplayer = document.createElement('i')
+    let ipc = document.createElement('i')
+    for (let x = 0; x < pc.length; x ++){
+        ipc.classList.add(pc[x])
+        iplayer.classList.add(player[x])
+    }
+    let players = []
+    players.push(iplayer, ipc)
+    if (players.length == 0){
+        playerDiv.appendChild(players[0])
+        pcDiv.appendChild(players[1])
+    }
+    players = []
+    
+}
 
 
+// displayPC.append(rock)
+
+// const whoWon = function(playerWon){
+//     let result;
+//     if (playerWon == "tie"){
+//         result = "TIE!"
+//     } else if (playerWon) {
+//         result = "Player Won!"
+//     } else {
+//         result = "PC Won!"
+//     }
+//     return result
+// }
+
+
+    
+// function checkResult(userChoice, pcChoice) {
+//     if (userChoice === pcChoice) {
+//         return whoWon("tie");
+    
+//     } else if (userChoice === 3 && pcChoice === 1) {
+//         return whoWon(false)
+    
+//     } else if (userChoice === 1 && pcChoice === 3) {
+//         return whoWon(true)
+    
+//     } else if (userChoice > pcChoice) {
+//         return whoWon(true)
+    
+//     } else {
+//         return whoWon(false)
+//     }
+    
+// }
 
 
 
